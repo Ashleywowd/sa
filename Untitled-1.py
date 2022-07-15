f4f49dee8c059df1f170be6dc5442aed9caf42ff
Ashleywowd <75246779+Ashleywowd@users.noreply.github.com>
2022-07-15 15:51:35 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+import time
+import os 
+import shutil
+
+def main():
+    days = 30
+    seconds = time.time() - (days * 24 * 60 * 60)
+
+    if os.path.exists(path):
+        for subfolders, folders, files in os.walk(path):
+        folder_path = os.path.join(root_folder, folder)
+        os.remove(path)
+
+        
